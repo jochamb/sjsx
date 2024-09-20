1cbea026b9ab2b948fa35f796913ff0e1e0a0ec4
jason <23511462+jochamb@users.noreply.github.com>
2024-09-20 15:38:41 -0500

    fix-bundling-1
    Changes committed:
    
    	new file:   .idea/inspectionProfiles/Project_Default.xml
    	new file:   .idea/saveactions_settings.xml
    	new file:   bundle.ts
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/index.ts
    	modified:   tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { JSX, jsxs, jsx, jsxDEV, Fragment } from './jsx-runtime';
+import { JSXNode, RenderedJSX, FunctionComponent } from './types';
+
+export { jsxs, jsx, jsxDEV, Fragment, RenderedJSX };
+export type { JSX, JSXNode, FunctionComponent };
